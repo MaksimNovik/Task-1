daa94d1a417fe40e3a0d318f3296388d170f58b6
MaksimNovik <maksimnovik299@gmail.com>
2024-03-25 17:14:05 +0300

    Реализация

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,22 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Мария"," Петрова",(byte)66);
+        userService.saveUser("Петр"," Петров",(byte)57);
+        userService.saveUser("Влад"," Владислав",(byte)52);
+        userService.saveUser("Елена","Смирнова",(byte)66);
+
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+
+    static String URL = "jdbc:postgresql://localhost:5432/postgres";
+    static String USERNAME = "postgres";
+    static String PASSWORD = "postgres";
+
+    public static Connection getConnectToDatabase() throws SQLException, ClassNotFoundException {
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
 }
+
+
